c0e8b4a7b386676b10d0e9d4fdd74edea3e711c6
Wangska <wangska1283@gmail.com>
2024-05-20 23:58:26 +0800

    prefinal

---
+++ src/pangpassragud/Pangpassragud.java
@@ -0,0 +1,17 @@
+
+package pangpassragud;
+
+/**
+ *
+ * @author Sofia
+ */
+public class Pangpassragud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
